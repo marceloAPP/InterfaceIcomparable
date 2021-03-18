c873f7a280b6a87df269c5821bec96a03cf38f47
marceloAPP <marcelo_freeday@yahoo.com.br>
2021-03-18 18:42:06 -0300

    Criando projeto

---
+++ Exerc_202/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Exerc_202
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\temp\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<string> list = new List<string>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+                    list.Sort();
+
+                    foreach (var item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Ocorreu um erro!" + e.Message); ;
+            }
+        }
+    }
+}
